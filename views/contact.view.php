3405efcbe524ee438a4c58731681eb90bc1e8743
kamal kafi <kamalkafi12@gmail.com>
2023-07-28 17:05:51 +0200

    refactor: add functions file

---
+++ views/contact.view.php
@@ -0,0 +1,35 @@
+<?php
+require('partials/head.php');
+require('partials/header.php');
+?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Contact</h1>
+      <?php 
+        require('partials/nav.php')
+      ?>
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+
+        <div class="col-lg-6">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Welcome</h5>
+              <p class="card-body">this is contact page this is contact page this is contact page this is contact page this is contact page .</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php
+require('partials/footer.php')
+?>
---
+++ views/partials/nav.php
@@ -0,0 +1,7 @@
+<nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a <?php echo urlIs('/') ? 'class="active"' : '' ?> href="/">Home</a></li>
+          <li class="breadcrumb-item"><a <?php echo urlIs('/about.php') ? 'class="active"' : '' ?> href="/about.php">About</a></li>
+          <li class="breadcrumb-item"><a <?php echo urlIs('/contact.php') ? 'class="active"' : '' ?> href="/contact.php">Contact</a></li>
+        </ol>
+</nav>
