95329caa68bdbfe72bd18ca48052776af8034bf8
kamal kafi <kamalkafi12@gmail.com>
2023-08-08 16:41:51 +0200

    use spl_autoload_register build in function to load Database and Response classes

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 const BASE_PATH = __DIR__. '/../';
 ini_set('display_errors',1);
 require BASE_PATH.'functions.php';
-require base_path('Database.php');
-require base_path('Response.php');
+
+
+spl_autoload_register(function($class){
+  require base_path($class . '.php');
+});
 require base_path('router.php');
 
 
