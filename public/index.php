015caac9b475319a583c59fd29dd2f531736f89e
kamal kafi <kamalkafi12@gmail.com>
2023-08-08 17:54:40 +0200

    refactor: group classess and function in Core directory

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 const BASE_PATH = __DIR__. '/../';
 ini_set('display_errors',1);
-require BASE_PATH.'functions.php';
+require BASE_PATH.'Core/functions.php';
 
 
 spl_autoload_register(function($class){
-  require base_path($class . '.php');
+  require base_path('Core/'.$class . '.php');
 });
-require base_path('router.php');
+require base_path('Core/router.php');
 
 
 
