873c1b6d6af94370fee8e24899ed4b601c4ae428
kamal kafi <kamalkafi12@gmail.com>
2023-12-08 16:21:36 +0200

    hash the password

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -40,7 +40,7 @@ if($user){
 
   $db->query('INSERT INTO users(email, password) VALUES (:email, :password)',[
     'email' => $email,
-    'password' => $password
+    'password' => password_hash($password,PASSWORD_BCRYPT)
   ]);
 
 
